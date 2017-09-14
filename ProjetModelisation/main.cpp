a285bcca751b2fffb9a7d61ab6f8c6d4818704e6
Dimitri Pierucci <dimitri.pierucci@ensimag.imag.fr>
2017-09-14 09:24:22 +0200

    print coucou

---
+++ ProjetModelisation/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: pieruccd
+ *
+ * Created on September 14, 2017, 9:20 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    std::cout << "coucou" << std::endl;
+    return 0;
+}
+
